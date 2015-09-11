5bb154a058f1af411f16138118057b45fd7d9129
Am Ting Nee <tingnee.am@my.experian.com>
2015-09-11 14:11:43 +0800

    updated R script for plot1

--- plot1.R
+++ plot1.R
@@ -1,11 +1,8 @@
 # Reading the data from file (assuming file has been downloaded, unzipped and copied to R home directory)
 hh.power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?")
 
-# Formatting the date
-hh.power$Date <- as.Date(hh.power$Date, format="%d/%m/%Y")
-
-# Subsetting the data using between "2007/02/01" to "2007/02/02"
-hh.power <- subset(hh.power, as.Date(Date) >= '2007/02/01'& as.Date(Date) <= '2007/02/02')
+# Subset the data 
+hh.power <- hh.power[hh.power$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # plot the graph in png format with 480X480
 png(filename="plot1.png", width=480, height=480)
